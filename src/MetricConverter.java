837aa55eb671d66cc4b7aef787e598db983428a9
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-25 16:40:31 -0400

    second commit

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class MetricConverter {
     public static void main(String[] args) {
+        double user = 0.0;
+        double miles = 0.0;
+        double feet = 0.0;
+        double inches = 0.0;
+        String trash = ""; // use for bad input which will read as a String
+        boolean done = false;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("What is the measurement you have in meters? ");
+            if (input.hasNextDouble()) {
+                // OK safe to read in a double
+                user = input.nextDouble();
+                input.nextLine(); // clears the newline from the buffer
+                done = true;  // because we got a valid number we can end the loop
+            } else {
+                // Not a double can’t use nextDouble() read as String with nextLine() instead
+                trash = input.nextLine();
+                System.out.println("You said you had " + trash);
+                System.out.println("Which is invalid");
+            }
+
+            miles = user * .00062137;
+            feet = user * 3.28084;
+            inches = user * 39.3701;
+            System.out.println("Meters to Miles: " + miles);
+            System.out.println("Meters to Feet: " + feet);
+            System.out.println("Meters to Inches: " + inches);
+
+
+        } while (false);
+
 
     }
 }
--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
 public class RectangleInfo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length = 0;
+        int width = 0;
+        int area = 0;
+        int diagonal = 0;
+        String trash = ""; // use for bad input which will read as a String
+        boolean done = false;
 
+
+        do {
+            System.out.println("What is the Legth? ");
+            if (input.hasNextDouble()) {
+                // OK safe to read in a double
+                length = input.nextInt();
+                input.nextLine(); // clears the newline from the buffer
+                done = true;  // because we got a valid number we can end the loop
+            } else {
+                // Not a double can’t use nextDouble() read as String with nextLine() instead
+                trash = input.nextLine();
+                System.out.println("You said you had " + trash);
+                System.out.println("Which is invalid");
+            }
+            System.out.println("What is the width? ");
+            if (input.hasNextDouble()) {
+                // OK safe to read in a double
+                width = input.nextInt();
+                input.nextLine(); // clears the newline from the buffer
+                done = true;  // because we got a valid number we can end the loop
+            } else {
+                // Not a double can’t use nextDouble() read as String with nextLine() instead
+                trash = input.nextLine();
+                System.out.println("You said you had " + trash);
+                System.out.println("Which is invalid");
+            }
+
+            area = length * width;
+
+            diagonal = (int) Math.sqrt((length*length)+(width*width));
+
+            System.out.println("The area is " + area );
+            System.out.println("The diagonal is " + diagonal);
+
+
+
+
+        }while(false);
     }
 }
