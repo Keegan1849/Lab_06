ea55c3316c678e6dea22863d7e9113c81d9c0a57
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-27 12:14:59 -0400

    third commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class CtoFConverter {
     public static void main(String[] args) {
@@ -6,26 +7,28 @@ public class CtoFConverter {
         double f;
         String trash = ""; // use for bad input which will read as a String
         boolean done = false;
+        String hi = "";
 
+        boolean stop = false;
         do {
             System.out.println("What is the temperature in C? ");
-            if (input.hasNextDouble()){
+            if (input.hasNextDouble()) {
                 // OK safe to read in a double
                 c = input.nextDouble();
-            input.nextLine(); // clears the newline from the buffer
-            done = true;  // because we got a valid number we can end the loop
+                input.nextLine(); // clears the newline from the buffer
+                done = true;  // because we got a valid number we can end the loop
                 f = (c * 1.8) + 32;
                 System.out.println("This is the temperature in F: " + f);
+            } else {
+                // Not a double can’t use nextDouble() read as String with nextLine() instead
+                trash = input.nextLine();
+                System.out.println("You said the temp was " + trash);
+                System.out.println("Which is invalid");
             }
-        else
-        {
-            // Not a double can’t use nextDouble() read as String with nextLine() instead
-            trash = input.nextLine();
-            System.out.println("You said the temp was " + trash);
-            System.out.println("Which is invalid");
-        }
 
 
-    }while (true);
+
+
+        } while (!false);
     }
 }
